6f281411a4ca8794fccba34bee821729747ab7d0
bob122001 <rberti1212@gmail.com>
2022-09-22 11:12:42 -0400

    m

--- learning.py
+++ learning.py
@@ -6,6 +6,7 @@ import numpy as np
 import tensorflow as tf
 import pickle
 
+
 dataset_path = '/home/robert/Pneumonia AI/chest_xray'
 train_path = '/home/robert/Pneumonia AI/chest_xray/train'
 val_path = '/home/robert/Pneumonia AI/chest_xray/val'
@@ -15,7 +16,7 @@ batch = 100
 
 num_classes = 2
 
-epochs = 2
+epochs = 3
 
 img_width, img_height = 128, 128
 
@@ -28,19 +29,24 @@ train = tf.keras.utils.image_dataset_from_directory(train_path, image_size=(img_
 
 validate = tf.keras.utils.image_dataset_from_directory(test_path, image_size=(img_height, img_width))
 
+test = tf.keras.utils.image_dataset_from_directory(val_path, image_size=(img_height, img_width))
+
 
 model = Sequential([
     normalization_layer,
+    Conv2D(16, kernel_size=(3, 3), activation='relu'),
+    MaxPooling2D(2, 2),
     Conv2D(32, kernel_size=(3, 3), activation='relu'),
-    MaxPooling2D(),
-    Dropout(0.5),
-    Conv2D(32, kernel_size=(3, 3), activation='relu'),
-    MaxPooling2D(),
-    Conv2D(32, kernel_size=(3, 3), activation='relu'),
-    MaxPooling2D(),
+    MaxPooling2D(2, 2),
+    Conv2D(64, kernel_size=(3, 3), activation='relu'),
+    MaxPooling2D(2, 2),
+    Conv2D(64, kernel_size=(3, 3), activation='relu'),
+    MaxPooling2D(2, 2),
+    Conv2D(64, kernel_size=(3, 3), activation='relu'),
+    MaxPooling2D(2, 2),
     Flatten(),
-    Dense(128, activation='relu'),
-    Dense(num_classes)
+    Dense(512, activation='relu'),
+    Dense(1, 'sigmoid')
 ])
 
 
@@ -50,6 +56,8 @@ model = Sequential([
 model.compile(optimizer='adam', loss=keras.losses.binary_crossentropy, metrics=['accuracy'])
 
 model.fit(train, batch_size=batch, epochs=epochs, verbose=1, validation_data=validate)
+
+model.evaluate(test)
 
 pickle.dump(model, open('model.sav', 'wb'))
 
